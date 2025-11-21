1410b96f71d0e3d7e0614d7ad003401e3ac787b4
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-11-21 12:20:26 -0300

    fix: remove chave de API padrão e validação de autorização

--- src/guards/api-key.guard.ts
+++ src/guards/api-key.guard.ts
@@ -11,13 +11,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 @Injectable()
 export class ApiKeyAuthGuard implements CanActivate {
-  private readonly apiKey = process.env.API_KEY || 'MINHA_API_KEY_SECRETA';
+  private readonly apiKey = process.env.API_KEY;
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
 
     const authHeader = request.headers['authorization'];
-    if (!authHeader || authHeader !== `Bearer ${this.apiKey}`) {
+    if (!authHeader) {
       throw new UnauthorizedException('API key inválida ou ausente');
     }
 
