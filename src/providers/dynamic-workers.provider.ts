12f01d75b5f7d9b716adfb7e582b0958616a4147
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-11-21 09:21:02 -0300

    fix: ajusta a concorrência dos workers para 1

--- src/providers/dynamic-workers.provider.ts
+++ src/providers/dynamic-workers.provider.ts
@@ -7,7 +7,7 @@ export function createDynamicWorkers(): Provider[] {
   const queues = [...ALL_TRT_QUEUES, 'pje-tst'];
 
   return queues.map((queueName) => {
-    const concurrency = 2;
+    const concurrency = 1;
 
     const processorOptions = {
       concurrency,
