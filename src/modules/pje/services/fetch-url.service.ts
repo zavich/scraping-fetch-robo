b07258093c90af1ecf674875c2f315961467a383
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-07 17:08:18 -0300

    refactor: torna a construção de cabeçalhos assíncrona e melhora a obtenção de cookies

--- src/modules/pje/services/fetch-url.service.ts
+++ src/modules/pje/services/fetch-url.service.ts
@@ -25,29 +25,31 @@ export class FetchUrlMovimentService {
     return Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
   }
 
-  private buildHeaders(
+  private async buildHeaders(
     numeroDoProcesso: string,
     instance: string,
     regionTRT: number,
     userAgent?: string,
   ) {
+    const ua =
+      userAgent || userAgents[Math.floor(Math.random() * userAgents.length)];
+    const redisKey = `aws-waf-token:${numeroDoProcesso}`;
+    const aws = await this.redis.get(redisKey);
     return {
       accept: 'application/json, text/plain, */*',
+      'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
       'content-type': 'application/json',
       'x-grau-instancia': instance,
-      cookie: 'ASSINADOR_PJE=PJEOFFICE; MO=PJEOFFICE',
+      cookie: `${aws}`,
       origin: `https://pje.trt${regionTRT}.jus.br`,
       referer: `https://pje.trt${regionTRT}.jus.br/consultaprocessual/detalhe-processo/${numeroDoProcesso}/${instance}`,
-      'user-agent':
-        userAgent ||
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
+      'user-agent': ua,
       'sec-fetch-site': 'same-origin',
       'sec-fetch-mode': 'cors',
       'sec-fetch-dest': 'empty',
-      'sec-ch-ua':
-        '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+      'sec-ch-ua': '"Chromium";v="120", "Not A(Brand";v="99"',
       'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"macOS"',
+      'sec-ch-ua-platform': '"Windows"',
     };
   }
 
@@ -76,7 +78,7 @@ export class FetchUrlMovimentService {
             `pje:token:captcha:${numeroDoProcesso}:${i}`,
           )) as string;
 
-          const headers = this.buildHeaders(
+          const headers = await this.buildHeaders(
             numeroDoProcesso,
             i.toString(),
             regionTRT,
@@ -169,7 +171,7 @@ export class FetchUrlMovimentService {
           ? userAgents[Math.floor(Math.random() * userAgents.length)]
           : undefined;
       const response = await axios.get<ProcessosResponse>(url, {
-        headers: this.buildHeaders(
+        headers: await this.buildHeaders(
           numeroDoProcesso,
           instance,
           regionTRT,
