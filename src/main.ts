ad05627c6d5f1194cbd1b198da361feb17f73124
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-24 15:26:50 -0300

    comenta a lógica de captura das filas TRTs no Bull Board

--- src/main.ts
+++ src/main.ts
@@ -35,13 +35,13 @@ async function bootstrap() {
     const bullQueues: Queue[] = [];
 
     // 24 TRTs
-    for (let i = 1; i <= 24; i++) {
-      const queueName = `BullQueue_pje-trt${i}`;
-      try {
-        const queue = app.get<Queue>(queueName);
-        if (queue) bullQueues.push(queue);
-      } catch {}
-    }
+    // for (let i = 1; i <= 24; i++) {
+    //   const queueName = `BullQueue_pje-trt${i}`;
+    //   try {
+    //     const queue = app.get<Queue>(queueName);
+    //     if (queue) bullQueues.push(queue);
+    //   } catch {}
+    // }
     for (let i = 1; i <= 24; i++) {
       const queueName = `BullQueue_pje-documentos-trt${i}`;
       try {
