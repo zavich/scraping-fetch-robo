819a765768eb2c0573de6af781b5d69fda24c119
rafaaelnicacio <rafaelnicacio154@gmail.com>
2026-04-24 12:48:51 -0300

    ajusta a lógica de concorrência dos trabalhadores dinâmicos para queues específicas e reduz o limite máximo de tarefas simultâneas

--- src/providers/dynamic-workers.provider.ts
+++ src/providers/dynamic-workers.provider.ts
@@ -7,14 +7,19 @@ export function createDynamicWorkers(): Provider[] {
   const queues = [...ALL_TRT_QUEUES, 'pje-tst'];
 
   return queues.map((queueName) => {
-    const concurrency = 3;
+    const concurrency =
+      queueName === 'pje-trt3' ||
+      queueName === 'pje-trt9' ||
+      queueName === 'pje-tst'
+        ? 1
+        : 3; // TST com concorrência 1, TRT com 3
 
     const processorOptions = {
       concurrency,
       lockDuration: 120000,
       stalledInterval: 30000,
       limiter: {
-        max: 6,
+        max: 3,
         duration: 1000,
       },
     };
